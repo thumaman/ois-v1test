085e5be986568866223ac076fef743fdee9fe1aa
Vid Thuma <vt0138@student.uni-lj.si>
2022-02-24 12:44:06 +0100

    Dodan izračun pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,22 @@
+import java.util.*;
 
-public class Gravitacija{
-    public static void main(String[] args){
-        System.out.println("OIS je zakon!");
+public class Gravitacija {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double C = 6.674 * Math.pow(10, -11);
+        double M = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
+
+        double v = sc.nextDouble();
+
+        double a = izracun(C, M, r, v);
+
+
     }
+
+    public static double izracun(double C, double M, double r, double v) {
+        return C * M / Math.pow((r + v), 2);
+    }
+
 }
